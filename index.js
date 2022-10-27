e7ca5475182d129407722e76e29de005758c31cd
Taqinur <taqi1662@gmail.com>
2022-10-28 00:44:35 +0600

    added data API

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
+app.use(cors());
+
+const courses = require('./data.json');
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
-})
+    res.send('API Running');
+});
+
+app.get('/course', (req, res) =>{
+    res.send(courses);
+});
+
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = courses.find(course => course.id == id);
+    res.send(selectedCourse);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log('Server running on port', port);
 })
